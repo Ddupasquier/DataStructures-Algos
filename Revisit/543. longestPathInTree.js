b5e07b2a43d95284b8df0f96f88c4d2737824851
dylandupasquier <dylandupasquier@gmail.com>
2022-06-29 21:13:07 -0700

    change to es6

--- Revisit/543. longestPathInTree.js
+++ Revisit/543. longestPathInTree.js
@@ -1,13 +1,14 @@
-var diameterOfBinaryTree = function (root) {
-      let path = 0;
-      function helper(root) {
-          if (!root) return -1;
-  
-          const left = helper(root.left) + 1;
-          const right = helper(root.right) + 1;
-          path = Math.max(path, left + right);
-          return Math.max(left, right);
-      }
-      helper(root);
-      return path;
+const diameterOfBinaryTree = (root) => {
+  let path = 0;
+  const helper = (root) => {
+    if (!root) return -1;
+
+    const left = helper(root.left) + 1;
+    const right = helper(root.right) + 1;
+    path = Math.max(path, left + right);
+    return Math.max(left, right);
   };
+  helper(root);
+
+  return path;
+};
