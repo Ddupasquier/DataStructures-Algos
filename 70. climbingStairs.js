a2c2f17c2b6aa913fe03cfd7bfaa67de013a2767
dylandupasquier <dylandupasquier@gmail.com>
2022-07-05 13:17:53 -0700

    .

---
+++ 70. climbingStairs.js
@@ -0,0 +1,10 @@
+const climbStairs = (n) => {
+  let dp = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+
+
+console.log(climbStairs(2)); // 2
