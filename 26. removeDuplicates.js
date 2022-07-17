74b9489daa9c0f87e55f3cd508882c552fc88bd7
dylandupasquier <dylandupasquier@gmail.com>
2022-07-17 09:54:51 -0700

    new stuff

--- 26. removeDuplicates.js
+++ 26. removeDuplicates.js
@@ -10,3 +10,4 @@ const removeDuplicates = (nums) => {
 
 console.log(removeDuplicates([1, 1, 2]));
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
--- 33. findNum.js
+++ 33. findNum.js
@@ -1,6 +1,10 @@
 const search = (nums, target) => {
-      for (let [index, num] of nums.entries()) {
-          if (num === target) return index
-      }
-      return -1
-  };
+  for (let [index, num] of nums.entries()) {
+    if (num === target) return index;
+  }
+  return -1;
+};
+
+console.log(search([1, 2, 3, 4, 5], 3));
+console.log(search([1, 2, 3, 4, 5], 6));
+console.log(search([1, 2, 3, 4, 5], 0));
